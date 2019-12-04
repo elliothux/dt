d2a6ece0c1a777dd0803a5dbae5c6a50897528e7
HuQingyang <i@do.codes>
2019-12-05 01:25:53 +0800

    feat: calc rotate deg

--- src/components/GraphController/Rotater.tsx
+++ src/components/GraphController/Rotater.tsx
@@ -9,31 +9,32 @@ interface Props {
   onMoveRotateEnd: (rotate: DEG) => void;
 }
 
+function pow2(i: number): number {
+  return i * i;
+}
+
 export class Rotater extends React.PureComponent<Props> {
-  public static calcRotate = (x: PX, y: PX): DEG => {
-    // if (x === 0 && y === 0) {
-    //   return 0;
-    // }
-
-    const deg = (180 / Math.PI) * Math.atan2(y + (y > 0 ? 1 : -1) * 30, x);
-
-    // if (x > 0) {
-    //   return 90 - deg;
-    // }
-    //
-    // if (x < 0) {
-    //   return deg - 90;
-    // }
-
-    return deg;
+  public static calcRotate = (x: PX, y: PX, w: PX, h: PX): DEG => {
+    const sinHalfA =
+      (0.5 * Math.sqrt(pow2(x) + pow2(y))) /
+      Math.sqrt(pow2(w * 0.5) + pow2(h * 0.5));
+    const halfA = Math.asin(sinHalfA);
+    return halfA * 2;
   };
 
-  private onMoveRotate = (moveX: PX, moveY: PX) => {
-    this.props.onMoveRotate(Rotater.calcRotate(moveX, moveY));
+  private onMoveRotate = (e: React.MouseEvent, moveX: PX, moveY: PX) => {
+    const rotate = Rotater.calcRotate(
+      moveX,
+      moveY,
+      this.props.width,
+      this.props.height
+    );
+    console.log(rotate);
+    this.props.onMoveRotate(rotate);
   };
 
   private onMoveRotateEnd = (moveX: PX, moveY: PX) => {
-    this.props.onMoveRotateEnd(Rotater.calcRotate(moveX, moveY));
+    // this.props.onMoveRotateEnd(Rotater.calcRotate(moveX, moveY));
   };
 
   // private onMoveStart
@@ -45,7 +46,7 @@ export class Rotater extends React.PureComponent<Props> {
       <>
         <rect className="rotate-controller top-left" x={-10} y={-10} />
 
-        <DragObserver>
+        <DragObserver onDrag={this.onMoveRotate}>
           <rect className="rotate-controller top-right" x={width - 8} y={-10} />
         </DragObserver>
 
